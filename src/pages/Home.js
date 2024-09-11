30e8c3f374becca4545e8214ba82adca4af60bde
james-chiou <jamesfgh963@gmail.com>
2024-09-11 19:55:20 +0800

    Initial commit

---
+++ src/pages/Home.js
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Search from "../components/Search";
+import Picture from "../components/Picture";
+
+const Home = () => {
+  // State Lifting
+  //原本是放在Search.js中，但為了將結果放到Picture.js中，因此要Lift到Home.js
+  let [input, setInput] = useState("");
+  let [data, setData] = useState(null);
+  let [page, setPage] = useState(1);
+  let [curSearch, setCurSearch] = useState("");
+  const auth = process.env.PEXELSAPIKEY;
+  const initialURL = "https://api.pexels.com/v1/curated?page=1&per_page=15";
+  let searchURL = `https://api.pexels.com/v1/search?query=${input}&per_page=15&page=1`;
+
+  const search = async (url) => {
+    let result = await axios.get(url, {
+      headers: { Authorization: auth },
+    });
+    //console.log(result);
+    setData(result.data.photos);
+    setCurSearch(input);
+  };
+
+  // Closure
+  const morePicture = async () => {
+    let newURL;
+    setPage(page + 1);
+    if (curSearch === "") {
+      newURL = `https://api.pexels.com/v1/curated?page=${page + 1}&per_page=15`;
+    } else {
+      newURL = `https://api.pexels.com/v1/search?query=${curSearch}&per_page=15&page=${
+        page + 1
+      }`;
+    }
+    let result = await axios.get(newURL, {
+      headers: { Authorization: auth },
+    });
+    setData(data.concat(result.data.photos));
+  };
+
+  useEffect(() => {
+    search(initialURL);
+  }, []);
+
+  return (
+    <div style={{ minHeight: "100vh" }}>
+      <Search
+        search={() => {
+          search(searchURL);
+        }}
+        setInput={setInput}
+      />
+      <div className="pictures">
+        {data &&
+          data.map((d) => {
+            return <Picture data={d} />;
+          })}
+      </div>
+      <div className="morePicture">
+        <button onClick={morePicture}>更多圖片</button>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
