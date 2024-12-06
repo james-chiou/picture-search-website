73fae1bdcc1717f44303d220b6faef9e1e8a31bc
james-chiou <jamesfgh963@gmail.com>
2024-12-06 17:28:36 +0800

    update-content

--- src/pages/About.js
+++ src/pages/About.js
@@ -10,13 +10,14 @@ const About = () => {
             <li className="col-md-10 fs-5">
               <ol>
                 <li className="col-md-10 fs-5 my-2">
-                  本專案使用React.js作為前端的開發框架，並透過axios套件來實現AJAX透過API向第三方網站請求資料。
+                  本專案使用React.js作為前端框架，並用AJAX實現透過API向第三方網站請求資料。
                 </li>
                 <li className="col-md-10 fs-5 my-2">
                   本專案的資料來源為Pexels網站，Pexels網站中的照片皆為免費授權。本專案向Pexels申請API
                   Key後，跟據Pexels API規範來設定參數。例如: API
                   URL中的頁數、每頁顯示照片數量等。
                 </li>
+                <img style={{ width: "70%" }} src="./project8-3.jpg" alt="" />
                 <li className="col-md-10 fs-5 my-2">
                   在網站中，使用者可以透過搜尋得到想要的照片，同時也可以下載想要的照片。
                 </li>
